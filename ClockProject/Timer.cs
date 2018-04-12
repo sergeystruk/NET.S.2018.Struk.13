b2fa35d74d3ee99eded7b276eedf36e3d1f10a36
Sergey <serik.privet@gmail.com>
2018-04-12 18:06:50 +0300

    Task without comments yet

---
+++ ClockProject/Timer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace ClockProject
+{
+    public class Timer
+    {
+        public event EventHandler<TimeEndedEventArgs> TimeEnded;
+
+        public void SetNewTimer(int miliseconds)
+        {
+            Console.WriteLine($"Timer set on {miliseconds} miliseconds");
+            OnTimeEnded(this, new TimeEndedEventArgs(miliseconds));
+            Console.WriteLine("Execution ended");
+        }
+
+        protected virtual void OnTimeEnded(object source, TimeEndedEventArgs e)
+        {
+            TimeEnded?.Invoke(this, e);
+        }
+    }
+
+    public sealed class TimeEndedEventArgs : EventArgs
+    {
+        private int miliseconds;
+        public int Miliseconds
+        {
+            get { return miliseconds; }
+        }
+
+        public TimeEndedEventArgs(int miliseconds)
+        {
+            this.miliseconds = miliseconds;
+        }
+    }
+
+    public sealed class Listener
+    {
+        public Listener() { }
+
+        public void Register(Timer timer)
+        {
+            timer.TimeEnded += WaitTime;
+        }
+
+        public void Unregister(Timer timer)
+        {
+            timer.TimeEnded -= WaitTime;
+        }
+
+        public void WaitTime(object source, TimeEndedEventArgs eventArgs)
+        {
+            Thread.Sleep(eventArgs.Miliseconds);
+        }
+    }
+}
---
+++ ClockProjectTestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ClockProject;
+
+namespace ClockProjectTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+
+            Listener listener = new Listener();
+            listener.Register(timer);
+
+            timer.SetNewTimer(5000);
+        }
+    }
+}
